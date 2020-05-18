3c265fb79b37355c4c3af97a631e9c474a0a9c3e
Nazanin Azarian <badi.mojgan@gmail.com>
2020-05-18 21:11:27 +0430

    SaveManager fields

--- src/main/java/Repository/SaveManager.java
+++ src/main/java/Repository/SaveManager.java
@@ -1,4 +1,18 @@
 package Repository;
 
+import Models.Status;
+
 public class SaveManager {
+    private int userId;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private Status status;
+    private static int lastId = 0;
+
+    private SaveManager() {
+    }
 }
