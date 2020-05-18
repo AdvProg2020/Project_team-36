30a4bb39dc14c2fbee7fce9e485ce1f2cc010588
Nazanin Azarian <badi.mojgan@gmail.com>
2020-05-18 22:44:52 +0430

    SaveDiscount.save()

--- src/main/java/Repository/SaveDiscount.java
+++ src/main/java/Repository/SaveDiscount.java
@@ -2,6 +2,8 @@ package Repository;
 
 import Models.Customer;
 import Models.Discount;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -21,6 +23,19 @@ public class SaveDiscount {
         this.customersIncludedIds = new ArrayList<>();
     }
 
+    public static void save(Discount discount){
+        SaveDiscount saveDiscount = new SaveDiscount();
+        saveDiscount.id = discount.getId();
+        saveDiscount.startTime = discount.getStartTime();
+        saveDiscount.endTime = discount.getEndTime();
+        saveDiscount.discountPercent = discount.getDiscountPercent();
+        saveDiscount.repetitionForEachUser = discount.getRepetitionForEachUser();
+        discount.getCustomersIncluded().forEach(customer -> saveDiscount.customersIncludedIds.add(customer.getUserId()));
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String saveDiscountGson = gson.toJson(saveDiscount);
+        FileUtil.write(FileUtil.generateAddress(Discount.class.getName(),saveDiscount.id),saveDiscountGson);
+    }
     public static Discount load(int id){
         return null;
     }
