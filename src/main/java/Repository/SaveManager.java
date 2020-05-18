1becf85f2a30a86b1f9b6c0963655b757fe24190
Nazanin Azarian <badi.mojgan@gmail.com>
2020-05-18 21:17:00 +0430

    SaveManager.save()

--- src/main/java/Repository/SaveManager.java
+++ src/main/java/Repository/SaveManager.java
@@ -1,6 +1,9 @@
 package Repository;
 
+import Models.Manager;
 import Models.Status;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class SaveManager {
     private int userId;
@@ -14,5 +17,21 @@ public class SaveManager {
     private static int lastId = 0;
 
     private SaveManager() {
+    }
+
+    public static void save(Manager manager){
+        SaveManager saveManager = new SaveManager();
+        saveManager.userId = manager.getUserId();
+        saveManager.username = manager.getUsername();
+        saveManager.firstname = manager.getFirstname();
+        saveManager.lastname = manager.getLastname();
+        saveManager.email = manager.getEmail();
+        saveManager.phoneNumber = manager.getPhoneNumber();
+        saveManager.password = manager.getPassword();
+        saveManager.status = manager.getStatus();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String saveManagerGson = gson.toJson(saveManager);
+        FileUtil.write(FileUtil.generateAddress(Manager.class.getName(),saveManager.userId),saveManagerGson);
     }
 }
