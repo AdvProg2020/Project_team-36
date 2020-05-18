8f1363e43b5c5b11364a0db7e731cf59234d3e35
Nazanin Azarian <badi.mojgan@gmail.com>
2020-05-18 18:06:26 +0430

    SaveCustomer.save()

--- src/main/java/Repository/SaveSelectedItem.java
+++ src/main/java/Repository/SaveSelectedItem.java
@@ -3,6 +3,10 @@ package Repository;
 import Models.SelectedItem;
 
 public class SaveSelectedItem {
+
+    public SaveSelectedItem(SelectedItem selectedItem) {
+    }
+
     public SelectedItem generateSelectedItem(){
         return null;
     }
