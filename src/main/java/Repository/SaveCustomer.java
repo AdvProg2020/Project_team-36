90890ce57a83ee38b79e5432a7d9a74dedbc6f38
Nazanin Azarian <badi.mojgan@gmail.com>
2020-05-18 18:06:26 +0430

    SaveCustomer.save()

--- src/main/java/Repository/SaveCustomer.java
+++ src/main/java/Repository/SaveCustomer.java
@@ -1,6 +1,8 @@
 package Repository;
 
 import Models.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,6 +28,26 @@ public class SaveCustomer {
     private SaveCustomer() {
         this.allCustomerLogIds = new ArrayList<>();
         this.allDiscountsForCustomer = new HashMap<>();
+        this.cart = new ArrayList<>();
+    }
+
+    public static void save(Customer customer){
+        SaveCustomer saveCustomer = new SaveCustomer();
+        saveCustomer.userId = customer.getUserId();
+        saveCustomer.username = customer.getUsername();
+        saveCustomer.firstname = customer.getFirstname();
+        saveCustomer.lastname = customer.getLastname();
+        saveCustomer.email = customer.getEmail();
+        saveCustomer.phoneNumber = customer.getPhoneNumber();
+        saveCustomer.password = customer.getPassword();
+        saveCustomer.status = customer.getStatus();
+        saveCustomer.credit = customer.getCredit();
+        customer.getAllLogs().forEach(customerLog -> saveCustomer.allCustomerLogIds.add(customerLog.getId()));
+        customer.getCart().forEach(selectedItem -> saveCustomer.cart.add(new SaveSelectedItem(selectedItem)));
+        customer.getAllDiscountsForCustomer().forEach((key,value) -> saveCustomer.allDiscountsForCustomer.put(key.getId(),value));
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String saveCustomerGson = gson.toJson(saveCustomer);
+        FileUtil.write(FileUtil.generateAddress(Customer.class.getName(),saveCustomer.userId),saveCustomerGson);
     }
 
     public static Customer load(int id){
