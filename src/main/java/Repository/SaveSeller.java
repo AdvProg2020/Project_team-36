c47016dcdd5339ca6f82058c0c56276f86925f2d
Nazanin Azarian <badi.mojgan@gmail.com>
2020-05-18 20:25:44 +0430

    SaveSeller fields

--- src/main/java/Repository/SaveSeller.java
+++ src/main/java/Repository/SaveSeller.java
@@ -1,8 +1,34 @@
 package Repository;
 
-import Models.Seller;
+import Models.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SaveSeller {
+    private int userId;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private Status status;
+    private static int lastId = 0;
+
+    private long credit;
+    private String companyName;
+    private String companyInfo;
+    private List<Integer> allSellerLogIds;
+    private List<Integer> allProductIds;
+    private List<Integer> allOffIds;
+
+    private SaveSeller() {
+        this.allOffIds = new ArrayList<>();
+        this.allProductIds = new ArrayList<>();
+        this.allSellerLogIds = new ArrayList<>();
+    }
+
     public static Seller load(int id){
         return null;
     }
