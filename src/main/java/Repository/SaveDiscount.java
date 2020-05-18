272cf538c814aed9f80c91015c93043e7857144c
Nazanin Azarian <badi.mojgan@gmail.com>
2020-05-18 22:38:49 +0430

    SaveDiscount fields

--- src/main/java/Repository/SaveDiscount.java
+++ src/main/java/Repository/SaveDiscount.java
@@ -1,8 +1,26 @@
 package Repository;
 
+import Models.Customer;
 import Models.Discount;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class SaveDiscount {
+    private int id;
+    private Date startTime;
+    private Date endTime;
+    private double discountPercent;//bar hasbe darsad nist
+    private long discountLimit;
+    private int repetitionForEachUser;
+    private static int lastId = 0;
+    private List<Integer> customersIncludedIds;
+
+    private SaveDiscount() {
+        this.customersIncludedIds = new ArrayList<>();
+    }
+
     public static Discount load(int id){
         return null;
     }
