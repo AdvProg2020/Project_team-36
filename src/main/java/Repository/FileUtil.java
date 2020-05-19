dcee32256cc49b7740b2f8362e14b4c8762903ca
Nazanin Azarian <badi.mojgan@gmail.com>
2020-05-20 02:18:32 +0430

    Write Json To File

--- src/main/java/Repository/FileUtil.java
+++ src/main/java/Repository/FileUtil.java
@@ -1,8 +1,19 @@
 package Repository;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileUtil {
     public static void write(String address, String data){
-
+        try {
+            FileWriter myWriter = new FileWriter(address);
+            myWriter.write(data);
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
     public static String read(String address){
@@ -11,6 +22,10 @@ public class FileUtil {
     }
 
     public static String generateAddress(String className, int id){
-        return "/resources/"+className+"/"+id+".json";
+        File file = new File("./src/main/resources/"+className+"/");
+        if(!file.exists()){
+            file.mkdirs();
+        }
+        return "./src/main/resources/"+className+"/"+id+".json";
     }
 }
