db0a8b10159ff042ad029bbfacc4b85b0cb0d642
Nazanin Azarian <badi.mojgan@gmail.com>
2020-05-21 00:42:57 +0430

    SaveRequest.save()

--- src/main/java/Repository/SaveRequest.java
+++ src/main/java/Repository/SaveRequest.java
@@ -1,7 +1,8 @@
 package Repository;
 
-import Models.Pendable;
-import Models.Status;
+import Models.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class SaveRequest {
     private int requestId;
@@ -14,4 +15,28 @@ public class SaveRequest {
 
     private static int lastId;
 
+    private SaveRequest() {
+    }
+
+    public static void save(Request request) {
+        SaveRequest saveRequest = new SaveRequest();
+        saveRequest.requestId = request.getRequestId();
+        saveRequest.status = request.getStatus();
+
+        if (request.getPendableRequest() instanceof Product) {
+            saveRequest.saveProduct = new SaveProduct((Product) request.getPendableRequest());
+        } else if (request.getPendableRequest() instanceof ProductField) {
+            saveRequest.saveProductField = new SaveProductField((ProductField) request.getPendableRequest());
+        } else if (request.getPendableRequest() instanceof Comment) {
+            saveRequest.saveComment = new SaveComment((Comment) request.getPendableRequest());
+        } else if (request.getPendableRequest() instanceof Seller) {
+            saveRequest.saveSeller = new SaveSeller((Seller) request.getPendableRequest());
+        } else if (request.getPendableRequest() instanceof Sale) {
+            saveRequest.saveSale = new SaveSale((Sale) request.getPendableRequest());
+        }
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String saveRequestGson = gson.toJson(saveRequest);
+        FileUtil.write(FileUtil.generateAddress(Request.class.getName(),saveRequest.requestId),saveRequestGson);
+    }
 }
