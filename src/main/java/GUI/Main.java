95be227e135c9efda648fb71e9a8b987b24e61ec
karanehk <keypoor.karaneh@gmail.com>
2020-06-13 16:43:34 +0430

    add setControllers

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -1,15 +1,14 @@
 package GUI;
 
 import Models.User;
-import View.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import java.util.Scanner;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -18,6 +17,7 @@ public class Main extends Application {
 //        Menu.setControllers();
 //        Menu runMenu = new MainMenu();
 //        runMenu.execute();
+        Constants.setControllers();
         launch(args);
 
     }
@@ -27,6 +27,7 @@ public class Main extends Application {
         Constants.getGuiManager().setStage(stage);
         FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("/fxml/test.fxml"));
         Parent parent = fxmlLoader.load();
+       // stage.setMaximized(true);
         stage.setScene(new Scene(parent));
         Constants.getGuiManager().addToOpenedParents(parent);
         stage.show();
