5941365851909a69ba15a9f5aee35e021d77800f
karanehk <keypoor.karaneh@gmail.com>
2020-07-17 17:33:34 +0430

    NewProductController processQuery

--- Server/src/main/java/Controllers/NewProductController.java
+++ Server/src/main/java/Controllers/NewProductController.java
@@ -50,14 +50,14 @@ public class NewProductController {
         throw new InvalidCategoryName();
     }
 
-    public void setCategory(Category category){
-        this.category = category;
-        //neededFields.addAll(category.getAllFields());
-    }
-
-    public ArrayList<Field> getNeededFields(){
-        return neededFields;
-    }
+//    public void setCategory(Category category){
+//        this.category = category;
+//        //neededFields.addAll(category.getAllFields());
+//    }
+//
+//    public ArrayList<Field> getNeededFields(){
+//        return neededFields;
+//    }
 
     public void setEachCategoryField(String value, Field field) throws InvalidFieldValue{
         if(field instanceof IntegerField){
@@ -92,11 +92,11 @@ public class NewProductController {
         this.productField = new ProductField(price,seller,supply,0);
     }
 
-    public void setProductField(long price, int supply, Seller seller) {
-        this.productField = new ProductField(price,seller,supply,0);
-    }
+//    public void setProductField(long price, int supply, Seller seller) {
+//        this.productField = new ProductField(price,seller,supply,0);
+//    }
 
-    public void setProductionDate() {
+    private void setProductionDate() {
         Date now = new Date();
         SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
         try {
@@ -121,6 +121,94 @@ public class NewProductController {
         this.imagePath = imagePath;
     }
 
+    public Response processQuery(Query query) {
+        switch (query.getMethodName()) {
+            case "setName":
+                return processSetName(query);
+
+            case "setCompany":
+                return processSetCompany(query);
+
+            case "setCategory":
+                return processSetCategory(query);
+
+            case "setEachCategoryField":
+                return processSetEachCategoryField(query);
+
+            case "setInformation":
+                return processSetInformation(query);
+
+            case "setProductField":
+                return processSetProductField(query);
+
+            case "sendNewProductRequest":
+                return processSendNewProductRequest();
+
+            case "setImage":
+                return processSetImage(query);
+
+            default:
+                return new Response("Error", "");
+        }
+    }
+
+    private Response processSetName(Query query){
+        setName(query.getMethodInputs().get("name"));
+        return new Response("void", "");
+    }
+
+    private Response processSetCompany(Query query){
+        setCompany(query.getMethodInputs().get("company"));
+        return new Response("void", "");
+    }
+
+    private Response processSetCategory(Query query){
+        try {
+            setCategory(query.getMethodInputs().get("categoryName"));
+            return new Response("void", "");
+        } catch (InvalidCategoryName invalidCategoryName) {
+            return new Response("InvalidCategoryName", "");
+        }
+    }
+
+    private Response processSetEachCategoryField(Query query){
+        String fieldName = query.getMethodInputs().get("fieldName");
+        Field field = neededFields.get(0);
+        for (Field neededField : neededFields) {
+            if(neededField.getName().equals(fieldName)){
+                field = neededField;
+            }
+        }
+        try {
+            setEachCategoryField(query.getMethodInputs().get("value"), field);
+            return new Response("void", "");
+        } catch (InvalidFieldValue invalidFieldValue) {
+            return new Response("InvalidFieldValue", "");
+        }
+    }
+
+    private Response processSetInformation(Query query){
+        setInformation(query.getMethodInputs().get("information"));
+        return new Response("void", "");
+    }
+
+    private Response processSetProductField(Query query){
+        int supply = Integer.parseInt(query.getMethodInputs().get("supply"));
+        long price = Long.parseLong(query.getMethodInputs().get("price"));
+        setProductField(price, supply);
+        return new Response("void", "");
+    }
+
+    private Response processSendNewProductRequest(){
+        sendNewProductRequest();
+        return new Response("void", "");
+    }
+
+    private Response processSetImage(Query query){
+        setImage(query.getMethodInputs().get("imagePath"));
+        return new Response("void", "");
+    }
+    
     public static class InvalidCategoryName extends Exception{
 
     }
