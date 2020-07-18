1fd243b3807c04d7956b2ad8670395bb3670044e
Nazanin Azarian <badi.mojgan@gmail.com>
2020-07-19 02:16:20 +0430

    client process

--- Client/src/main/java/Network/Client.java
+++ Client/src/main/java/Network/Client.java
@@ -2,10 +2,38 @@ package Network;
 
 import Models.Query;
 import Models.Response;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
-    public static Response process (Query query){
-        //todo
-        return null;
+    public static Response process(Query query) {
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", 8080);
+            Gson gson = new GsonBuilder().create();
+            String output = gson.toJson(query);
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF(output);
+            String input = dataInputStream.readUTF();
+            socket.close();
+            Gson gson1 = new Gson();
+            return gson.fromJson(input, Response.class);
+        } catch (IOException e) {
+            if (socket != null && !socket.isClosed()){
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+            e.printStackTrace();
+            return null;
+        }
     }
 }
